0b0b984f2bb9e118a7cdf511a5d936ca3da4002c
Berkeley Almand-Hunter <berkeleyalmand-hunter@Berkeleys-MacBook-Pro.local>
2020-05-28 12:35:54 -0400

    update data

--- covid_plots.py
+++ covid_plots.py
@@ -176,7 +176,7 @@ def create_hospital_assets_dict():
                             '2020-05-09', '2020-05-10', '2020-05-11', '2020-05-12', '2020-05-13',
                             '2020-05-14', '2020-05-15', '2020-05-16', '2020-05-17', '2020-05-18',
                             '2020-05-19', '2020-05-20', '2020-05-21', '2020-05-22', '2020-05-23',
-                            '2020-05-24', '2020-05-25', '2020-05-26', '2020-05-27'],
+                            '2020-05-24', '2020-05-25', '2020-05-26', '2020-05-27', '2020-05-28'],
                          'available_icu_beds': [56, None, None, None, 77,
                                                 83, 86, 86, None, None,
                                                 92, 90, 124, 122, 110,
@@ -190,7 +190,7 @@ def create_hospital_assets_dict():
                                                 141, 158, 157, None, None,
                                                 163, None, 174, None, None,
                                                 185, 167, None, 164, None,
-                                                165, None, 157, 151],
+                                                165, None, 157, 151, 148],
                          'total_icu_beds':[135, None, None, None, None,
                                            151, 151, 164, None, None,
                                            176, 190, 272, 285, 289,
@@ -204,7 +204,7 @@ def create_hospital_assets_dict():
                                            304, 288, 307, None, None,
                                            361, None, 360, None, None,
                                            395, 388, None, 392, None,
-                                           391, None, 394, 406],
+                                           391, None, 394, 406, 408],
                          'available_ventilators':[218, None, None, None, 248,
                                                   248, 250, 247, None, None,
                                                   253, 262, 271, 266, 267,
@@ -218,7 +218,7 @@ def create_hospital_assets_dict():
                                                   220, 216, 224, None, None,
                                                   230, None, 250, None, None,
                                                   253, 253, None, 248, None,
-                                                  242, None, 238, 232],
+                                                  242, None, 238, 232, 233],
                          'total_ventilators':[291, None, None, None, None,
                                               306, 307, 308, None, None,
                                               309, 330, 348, 334, 324,
@@ -232,7 +232,7 @@ def create_hospital_assets_dict():
                                               317, 308, 310, None, None,
                                               313, None, 311, None, None,
                                               314, 316, None, 318, None,
-                                              316, None, 315, 315],
+                                              316, None, 315, 315, 319],
                          'alternative_ventilators':[None, None, None, None, None,
                                                     None, None, 58, None, None,
                                                     87, 89, 128, 186, 199,
@@ -246,7 +246,7 @@ def create_hospital_assets_dict():
                                                     413, 413, 413, None, None,
                                                     416, None, 426, None, None,
                                                     439, 439, None, 439, None,
-                                                    439, None, 439, 439]
+                                                    439, None, 439, 439, 441]
                         }
     # Calculate the number of occupied ICU Beds
     hosp_assets_dict = find_occupied_assets(hosp_assets_dict, 'total_icu_beds',
@@ -375,7 +375,7 @@ def plot_race_and_ethnicity(size):
                                         print_values=True,
                                         print_values_position='top'
                                         )
-        bar_chart.title = 'Case Rate by Race and Ethnicity'
+        bar_chart.title = 'Case Rate by Race'
         bar_chart.x_labels = df_ethnicity.race
         bar_chart.add('% of COVID Cases', df_ethnicity.perc_covid_cases.to_list())
         bar_chart.add('% of Maine Population', df_ethnicity.perc_maine_pop.to_list())
@@ -455,7 +455,7 @@ def plot_age_range(size):
     #create a df
     df_age = pd.DataFrame.from_dict({'age_range':['< 20','20s', '30s', '40s',
                                                   '50s', '60s', '70s','80+'],
-                                 'cases': [98,269,290,327,392,298,228,235]})
+                                 'cases': [101,281,298,339,402,302,230,236]})
     # add up the total cases and find % of total in each age range
     total_count = df_age.cases.sum()
     df_age['percent_of_tot'] = df_age.cases/total_count*100
@@ -467,7 +467,7 @@ def plot_age_range(size):
                           show_legend=False,
                           y_title='Percent of Cases (%)',
                           x_title='Age Group')
-    bar_chart.title = 'Case Distribution by Patient Age (May 27, 2020)'
+    bar_chart.title = 'Case Distribution by Patient Age (May 28, 2020)'
     bar_chart.x_labels = df_age.age_range
     bar_chart.add('% of Cases', df_age.percent_of_tot.to_list())
 
